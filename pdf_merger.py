f25c3da6abd69059c909d3dce6e98a0a031b2a59
Sanorian <krajnov.sascha@yandex.ru>
2023-06-09 08:46:34 +0400

    Add comments and fix README

--- pdf_merger.py
+++ pdf_merger.py
@@ -1,16 +1,23 @@
+# import of required modules
 from pypdf import PdfMerger
 import os
+# get path to the directory with .py script
 current_dir = os.getcwd()
-pdf_list = []
-os.chdir(f'{current_dir}\\pdfs')
-files = os.listdir()
-for file in files:
-    if file.split('.')[1]=='pdf':
-        pdf_list.append(file)
+# create an array for merging
 merger = PdfMerger()
-for pdf in pdf_list:
-    merger.append(pdf)
-split_dir = current_dir.split('\\')
+# changing directory to the directory where the PDF files are located
+os.chdir(f'{current_dir}\\pdfs')
+# get list of file names
+files = os.listdir()
+# check files for pdf ending
+for file in files:
+    try:
+        if file.split('.')[1]=='pdf':
+            merger.append(file)
+    except:...
+# return to the main directory
 os.chdir(f'{current_dir}')
+# merging to one file
 merger.write("result.pdf")
+# That's all!
 merger.close()
